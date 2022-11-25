bdb519a2c25e6aefe3cb81d246b0b9522103ab40
barysevich_k <borisevich711@gmail.com>
2022-11-25 13:01:59 +0300

    Add ru AM/D/LM in project

--- src/main/java/TranscriberDemo.java
+++ src/main/java/TranscriberDemo.java
@@ -4,11 +4,12 @@ import edu.cmu.sphinx.api.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-
+import java.net.URL;
+//Результат: илья ильф евгений петров золотой телёнок
 public class TranscriberDemo {
-    private static String ACOUSTIC_MODELS = "file:///C:/zero/zero_ru.cd_cont_4000";
-    private static String LANGUAGE_MODEL  = "file:///C:/zero/ru.lm";
-    private static String DICTIONARY      = "file:///C:/zero/ru.dic";
+    private static String ACOUSTIC_MODELS = "file:///D:/Project/TestJavaSphinx/sphinx/ru4/split/msu_ru_zero.cd_cont_2000";
+    private static String LANGUAGE_MODEL  = "file:///D:/Project/TestJavaSphinx/sphinx/ru.lm";
+    private static String DICTIONARY      = "file:///D:/Project/TestJavaSphinx/sphinx/ru.dic";
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
         configuration.setAcousticModelPath( ACOUSTIC_MODELS );
